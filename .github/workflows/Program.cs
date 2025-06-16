bb2b0aa54d09fde4a74a7e1aab34e84cd5fbdfcd
DiegoSSanchez <144294482+TheDiegoSanchez@users.noreply.github.com>
2025-06-16 15:23:11 -0500

    Update Program.cs

--- .github/workflows/Program.cs
+++ .github/workflows/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Configurar la aplicación
 var app = builder.Build();
 
+// Responder a la raíz
 app.MapGet("/", () => "Hola Mundo desde .NET en Vercel!");
 
 app.Run();
