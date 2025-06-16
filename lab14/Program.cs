5cd921a9250a0c5c0ad2e80e6a022108b4bbc36c
Diego Saul Sanchez Mamani <sanchezmamanidiegosaul@gmail.com>
2025-06-16 14:53:56 -0500

    FUNCIONAAAA

--- lab14/Program.cs
+++ lab14/Program.cs
@@ -1,9 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Crear el app web
 var app = builder.Build();
 
-// Responder a la ruta raíz
 app.MapGet("/", () => "Hola Mundo desde .NET en Vercel!");
 
 app.Run();
